c33cec6080e013a322c062671b278599bcc08058
Max Sharing <revcheg0@gmail.com>
2023-04-04 18:49:57 +0300

    Продовжуємо роботу

---
+++ source/js/script.js
@@ -0,0 +1,11 @@
+// Theme button
+(function () {
+  let themeButtons = document.querySelectorAll('.footer__theme');
+  let mainBody = document.querySelector('body');
+
+  for (var i = 0; i < themeButtons.length; i++) {
+    themeButtons[i].addEventListener('click', function() {
+      
+    });
+  };
+})();
