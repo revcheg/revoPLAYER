e0728eae39c652469e2bf9f17aae6cafe897e7d4
Max Sharing <revcheg0@gmail.com>
2023-05-08 20:44:10 +0300

    Додав час прокрутки

--- source/js/components/video.js
+++ source/js/components/video.js
@@ -2,6 +2,33 @@
 let progressInterval;
 let playbackQuality
 
+// Duration
+const videoPassed = CONTROLS.querySelector('.control__passed');
+const videoLeft = CONTROLS.querySelector('.control__left');
+
+function formatTime (timeInSeconds) {
+  let hours = Math.floor(timeInSeconds / 3600);
+  let minutes = Math.floor((timeInSeconds - (hours * 3600)) / 60);
+  let seconds = Math.floor(timeInSeconds - (hours * 3600) - (minutes * 60));
+  
+  if (hours < 10) {
+    hours = '0' + hours;
+  };
+
+  if (minutes < 10) {
+    minutes = '0' + minutes;
+  };
+
+  if (seconds < 10) {
+    seconds = '0' + seconds;
+  };
+
+  // minutes = minutes < 10 ? '0' + minutes : minutes;
+  // seconds = seconds < 10 ? '0' + seconds : seconds;
+
+  return hours + ':' + minutes + ':' + seconds;
+};
+
 function startProgress () {
   progressInterval = setTimeout(updateProgress, 1000);
 };
@@ -15,6 +42,11 @@ function updateProgress () {
   playbackQuality = VIDEO.getVideoPlaybackQuality();
   videoFPS = playbackQuality.totalVideoFrames / VIDEO.currentTime;
   statisticsFPS.innerHTML = videoFPS;
+
+  let currentVideoPassed = formatTime(videoCurrentTime); 
+  let currentVideoLeft = formatTime(videoDuration - videoCurrentTime); 
+  videoPassed.innerHTML = currentVideoPassed; 
+  videoLeft.innerHTML = currentVideoLeft; 
 
   startProgress();
   stayFocus();
